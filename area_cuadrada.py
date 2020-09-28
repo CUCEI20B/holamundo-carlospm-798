553fe5c7df291143b01e7acbd4e4e62d3f7fe79a
Carlos Paredes <carlos.paredes6900@alumnos.udg.mx>
2020-09-28 14:03:19 -0500

    Acabe el programa del área

---
+++ area_cuadrada.py
@@ -0,0 +1,10 @@
+"""
+Carlos Paredes Márquez.
+Área de un cuadrado.
+28/09/2020.
+"""
+print("Inserte un lado del cuadro: \n")
+lado = int(input("->    "))
+
+area_cuadrado = lado*lado
+print("El área del cuadrado es:\t", area_cuadrado)
